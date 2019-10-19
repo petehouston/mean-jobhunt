f00dbafe033be296abddd62591766d5e24870593
Pete Houston <contact@petehouston.com>
2019-10-18 21:32:58 -0500

    Fix salt round for bcrypt and add index to email field in user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
-const salt = process.env.BCRYPT_SALT || 10;
+const salt = parseInt(process.env.BCRYPT_SALT) || 10;
 
 const UserSchema = new Schema({
     name: {
@@ -12,6 +12,9 @@ const UserSchema = new Schema({
     },
     email: {
         type: String,
+        index: {
+            unique: true,
+        },
         trim: true,
         required: true,
     },
