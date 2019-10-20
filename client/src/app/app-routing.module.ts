9d62a085ce0d4bb14a1b55ea1d50fb2148006c0a
Pete Houston <contact@petehouston.com>
2019-10-20 15:53:39 -0500

    Add basic UI to add job page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {RegisterPageComponent} from "./register-page/register-page.component";
 import {JobListingPageComponent} from "./job-listing-page/job-listing-page.component";
 import {UserAddJobPageComponent} from "./user-add-job-page/user-add-job-page.component";
 import { ApplicantDetailsComponent} from './applicant-details/applicant-details.component';
+import {AuthGuard} from "./guards/auth.guard";
 
 
 const routes: Routes = [
@@ -17,7 +18,11 @@ const routes: Routes = [
   { path: 'jobs', component: JobListingPageComponent },
   { path: 'jobs/:job_id/application', component: ApplicantDetailsComponent },
 
-  { path: 'u/jobs/add', component: UserAddJobPageComponent },
+  { path: 'u/jobs/add', component: UserAddJobPageComponent,
+    canActivate: [
+      // AuthGuard,
+    ]
+  },
 
   { path: 'notfound', component: NotFoundComponent },
   { path: '**', redirectTo: 'notfound' }
--- client/src/app/user-add-job-page/user-add-job-page.component.ts
+++ client/src/app/user-add-job-page/user-add-job-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-user-add-job-page',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-add-job-page.component.css']
 })
 export class UserAddJobPageComponent implements OnInit {
+  addForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.addForm = this.createFormGroup();
+  }
+
+  createFormGroup() {
+    return this.formBuilder.group({
+
+    });
+  }
 
   ngOnInit() {
   }
