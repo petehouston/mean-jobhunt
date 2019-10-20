74335daebe9f6f76cd91a5bde0c5f209db14ba9d
Pete Houston <contact@petehouston.com>
2019-10-20 01:57:03 -0500

    Fix #33: update navbar upon auth status change

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogoutClicked() {
+    this.authService.logout();
+    this.router.navigate(['/', 'login']);
+  }
 }
