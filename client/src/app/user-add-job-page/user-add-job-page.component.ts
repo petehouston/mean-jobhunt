7d16eaf52f9720967ad8c0390cb04e00855c4e73
Pete Houston <contact@petehouston.com>
2019-10-20 16:02:23 -0500

    Add form group and controls

--- client/src/app/user-add-job-page/user-add-job-page.component.ts
+++ client/src/app/user-add-job-page/user-add-job-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-add-job-page',
@@ -8,6 +8,8 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class UserAddJobPageComponent implements OnInit {
   addForm: FormGroup;
+  error: string = null;
+  submitted: boolean = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.addForm = this.createFormGroup();
@@ -15,11 +17,31 @@ export class UserAddJobPageComponent implements OnInit {
 
   createFormGroup() {
     return this.formBuilder.group({
-
+      title: ['', Validators.required],
+      company: ['', Validators.required],
+      location: ['', Validators.required],
+      is_remote: 0,
+      visa_sponsor: 0,
+      job_type: 0,
+      salary_range: 0,
     });
   }
 
   ngOnInit() {
+  }
+
+  get f() {
+    return this.addForm.controls;
+  }
+
+  onSubmit() {
+    console.log(this.f.title.value);
+    console.log(this.f.company.value);
+    console.log(this.f.location.value);
+    console.log(this.f.is_remote.value);
+    console.log(this.f.visa_sponsor.value);
+    console.log(this.f.job_type.value);
+    console.log(this.f.salary_range.value);
   }
 
 }
