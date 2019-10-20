9a8289bed4f70a571669a0b2bef12225e5bce366
Pete Houston <contact@petehouston.com>
2019-10-20 01:37:00 -0500

    Integrate register form validation and request API

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: 'app-register-page',
@@ -8,13 +11,44 @@ import {FormGroup} from "@angular/forms";
 })
 export class RegisterPageComponent implements OnInit {
   registerForm: FormGroup;
-  constructor() { }
+  submitted: boolean = false;
+  error: string = null;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['/', 'my-jobs']);
+    }
+  }
 
   ngOnInit() {
+    this.registerForm = this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
 
+    this.authService.register(this.registerForm.controls.name.value, this.registerForm.controls.email.value, this.registerForm.controls.password.value)
+      .pipe(first())
+      .subscribe(result => {
+        if (result) {
+          this.router.navigate(['/', 'login']);
+          console.log(result);
+        } else {
+          this.error = 'Invalid input. Make sure you input data correctly.';
+        }
+      }, err => {
+        this.error = err.statusText;
+      })
   }
 
 }
