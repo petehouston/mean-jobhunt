1c662fd5c156bdcd2c5988ca9125c7b612c1e53f
Pete Houston <contact@petehouston.com>
2019-10-20 00:27:58 -0500

    Fix #12: add authentication service

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from "@angular/core";
+import {BehaviorSubject, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.get('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue() {
+    return this.currentUserSubject.value;
+  }
+
+  login(email: string, password: string) {
+    return this.http.post('http://localhost:4000/api/login', {email, password})
+      .pipe(
+        map(payload => {
+          const user = {email, access_token: payload['access_token'] };
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
+        })
+      );
+  }
+
+  register(name: string, email: string, password: string) {
+    return this.http.post('http://localhost:4000/api/register', {name, email, password})
+      .pipe(
+        map(payload => {
+          return payload['status'] === 'success';
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
