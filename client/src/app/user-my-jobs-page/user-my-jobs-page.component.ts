31fb24159a6b11342198972482f0dc8a378bc53b
Pete Houston <contact@petehouston.com>
2019-10-21 22:58:46 -0500

    Add icons and fix some UI

--- client/src/app/user-my-jobs-page/user-my-jobs-page.component.ts
+++ client/src/app/user-my-jobs-page/user-my-jobs-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserJobsService} from "../services/user_jobs.service";
+import { faClock, faMapMarker } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-my-jobs-page',
@@ -8,6 +9,8 @@ import {UserJobsService} from "../services/user_jobs.service";
 })
 export class UserMyJobsPageComponent implements OnInit {
   jobs: Array<Object> = [];
+  faClock = faClock;
+  faMapMarker = faMapMarker;
   constructor(private jobService: UserJobsService) {
     this.jobService.listJobs().subscribe(r => {
       this.jobs = r['payload']['jobs'];
