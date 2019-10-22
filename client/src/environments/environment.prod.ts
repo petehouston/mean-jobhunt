adee2288fbea2afe25584cc9c5c5e5de52f4b9e5
Pete Houston <contact@petehouston.com>
2019-10-22 14:56:22 -0500

    Add environment template for prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    basepath: `http://localhost:4000`,
+    routes: {
+      'auth': {
+        login: 'login',
+        register: 'register',
+      },
+      'api/jobs': {
+        latest: 'latest',
+        visa_sponsor: 'visa-sponsor',
+        remote_jobs: 'remote-jobs',
+        high_salary: 'high-salary',
+      }
+    }
+  }
 };
